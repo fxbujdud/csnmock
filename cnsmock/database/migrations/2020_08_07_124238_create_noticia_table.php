c7b7b33be0535772dc7cd7f6f92456b800ea2547
fxbujdud <fxbujdud@gmail.com>
2020-08-07 08:53:40 -0500

    proyecto: se añade informacion del proyecto

---
+++ cnsmock/database/migrations/2020_08_07_124238_create_noticia_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoticiaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noticia', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->longText('summary');
+            $table->longText('content');
+            $table->string('url_normalized');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noticia');
+    }
+}
